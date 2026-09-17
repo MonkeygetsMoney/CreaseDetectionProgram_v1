62e719b2fc3b4873c9b81b115bed1546b60f48ed
MonkeygetsMoney <thanh.baphung@gmail.com>
2026-09-16 20:56:55 -0500

    Passing Input

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print('hellooooo')
+from flask import Flask, render_template
+#from livereload import Server
+#the live reload is for running multiple thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    name = 'Toby Pie'
+    pizza_toppings = ['pepperoni', 'cheese', 'ham', 'pineapple']
+
+    return render_template('example.html', name = name, toppings = pizza_toppings)
+
+if __name__ == '__main__':
+    app.run(debug=True)
